ef05bfd492747f3f5c1b3e689caf9cf809d5d585
minh-le0205 <minh.levunguyen@gmail.com>
2024-11-06 20:57:23 +0700

    Create basic router

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <?php
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$segments = explode("/", $path);
 
+require "src/router.php";
 
-$action = $segments[2];
-$controller = $segments[1];
+$router = new Router();
+
+$router->add("/home/index", [
+  "controller" => "home",
+  "action" => "index"
+]);
+
+$router->add("/products", [
+  "controller" => "products",
+  "action" => "index"
+]);
+
+$router->add("/", [
+  "controller" => "home",
+  "action" => "index"
+]);
+
+$params = $router->match($path);
+
+if (!$params) {
+  exit("No route matched");
+}
+
+$action = $params['action'];
+$controller = $params['controller'];
 
 require "src/controllers/$controller.php";
 
